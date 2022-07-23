4b2618feaa6de333cb271dec50a875cc2c7f0637
Denish Sharma <denishsharma@ternaengg.ac.in>
2022-07-24 02:12:59 +0530

    Updating utils

--- app/utils/wallet.util.js
+++ app/utils/wallet.util.js
@@ -1,9 +1,20 @@
-const { web3 } = require('./web3.utils');
+const { web3 } = require("./web3.utils");
+const { lockUserAddress } = require("./secure.util");
 
 exports.createTransaction = async (transactionObject, secretKey) => {
     return await web3.eth.accounts.signTransaction(transactionObject, secretKey);
-}
+};
 
 exports.sendSignedTransaction = async (signedTransaction) => {
     return await web3.eth.sendSignedTransaction(signedTransaction.rawTransaction);
-}
+};
+
+exports.createLockedAccount = async (userKey) => {
+    const accountInfo = await web3.eth.accounts.create();
+    const accountAttributes = {
+        userKey,
+        address: accountInfo.address,
+        secret: accountInfo.privateKey,
+    };
+    return lockUserAddress(accountAttributes);
+};
--- app/utils/web3.utils.js
+++ app/utils/web3.utils.js
@@ -1,20 +1,21 @@
 require("dotenv").config();
 
 const Web3 = require("web3");
-const contract = require("@truffle/contract");
-let networkProvider = `${process.env.DEV_NETWORK_PROTOCOL}://${process.env.DEV_NETWORK_HOST}:${process.env.DEV_NETWORK_PORT}`;
+const protocol = process.env.DEV_NETWORK_PROTOCOL || 'ws';
+const networkProvider = `${protocol}://${process.env.DEV_NETWORK_HOST || 'localhost'}:${process.env.DEV_NETWORK_PORT || '7545'}`;
 
-let web3;
-if (typeof web3 !== "undefined") {
-    web3 = new Web3(web3.currentProvider);
-    console.log("Web3 initialized from current provider");
-} else {
-    let web3Provider = new Web3.providers.HttpProvider(networkProvider);
-    web3 = new Web3(web3Provider);
-    console.log("Web3 initialized from network provider");
+const web3 = new Web3();
+let provider;
+
+if (protocol === "http") {
+    provider = new Web3.providers.HttpProvider(networkProvider);
+} else if (protocol === "ws") {
+    provider = new Web3.providers.WebsocketProvider(networkProvider);
 }
+
+web3.setProvider(provider);
+console.log("Web3 initialized from network provider");
 
 module.exports = {
     web3,
-    contract
-}
+};
