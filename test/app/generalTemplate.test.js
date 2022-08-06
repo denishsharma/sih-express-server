b8983a3ad36625e7ab0efeed8fd20474d954aeef
Denish Sharma <denishsharma@ternaengg.ac.in>
2022-08-06 16:33:09 +0530

    test(app): configure test for app tests
    adding provision to run app tests, similar to unit tests
    
    Author: Denish Sharma

---
+++ test/app/generalTemplate.test.js
@@ -0,0 +1,83 @@
+const { Template, TemplateField } = require("../../app/sequelize");
+const bcrypt = require("bcrypt");
+
+const templateStructure = {
+    id: "",
+    name: "",
+    description: "",
+    version: "",
+    fields: {
+        fieldSignature: {
+            id: "",
+            name: "",
+            dataType: "",
+            position: 0,
+            isRequired: false,
+            isFixed: false,
+        },
+    },
+};
+
+const seedTemplates = async () => {
+    const templateAttributes = {
+        name: "Template 1",
+        description: "Template 1 Description",
+        version: "1.0.0",
+    };
+
+    const template = await Template.create(templateAttributes);
+
+    const templateFieldAttributes = [
+        {
+            templateId: template.id,
+            signature: bcrypt.hashSync("field1", 10),
+            name: "field1",
+            dataType: "string",
+            position: 1,
+            isRequired: true,
+            isFixed: true,
+        },
+        {
+            templateId: template.id,
+            signature: bcrypt.hashSync("field2", 10),
+            name: "field2",
+            dataType: "string",
+            position: 2,
+            isRequired: true,
+            isFixed: true,
+        },
+        {
+            templateId: template.id,
+            signature: bcrypt.hashSync("field3", 10),
+            name: "field3",
+            dataType: "string",
+            position: 3,
+            isRequired: true,
+            isFixed: true,
+        },
+    ];
+
+    for (const templateFieldAttribute of templateFieldAttributes) {
+        await TemplateField.create(templateFieldAttribute);
+    }
+};
+
+const someTest = async () => {
+    // await seedTemplates();
+
+    const templates = await Template.findAll({
+        include: [{ model: TemplateField }],
+    });
+
+    console.log(templates);
+};
+
+exports.test = async () => {
+    await someTest();
+
+    process.exit();
+};
+
+exports.metadata = {
+    name: "generalTemplate",
+};
--- test/index.js
+++ test/index.js
@@ -2,11 +2,15 @@ const appTests = {};
 const unitTests = {};
 const contractTests = {};
 
+// Import App Tests
+const generalTemplate = require("./app/generalTemplate.test");
+
 // Import Unit Tests
 const web3MetaTransactionCall = require("./unit/web3MetaTransactionCall.test");
 
 const args = process.argv.slice(2);
 const state = {
+    testAppSingle: args[0] === "test-app-single" || false,
     testUnitSingle: args[0] === "test-unit-single" || false,
 };
 
@@ -16,14 +20,21 @@ const addToTest = (test, list) => {
 };
 
 // Add to Tests
+addToTest(generalTemplate, appTests);
 addToTest(web3MetaTransactionCall, unitTests);
 
 const testUnitSingle = async (testName) => {
     await unitTests[testName]();
 };
 
+const testAppSingle = async (testName) => {
+    await appTests[testName]();
+};
+
 (async (state) => {
     if (state.testUnitSingle) {
         await testUnitSingle(args[1]);
+    } else if (state.testAppSingle) {
+        await testAppSingle(args[1]);
     }
 })(state);
