7db311dc665e75b492ffe4e934ea67553e4a0f15
Denish Sharma <denishsharma@ternaengg.ac.in>
2022-08-13 19:04:36 +0530

    update(general-template): update template controller
    some updates
    
    Author: Denish Sharma

--- app/controllers/template.controller.js
+++ app/controllers/template.controller.js
@@ -1,4 +1,4 @@
-const { Template, TemplateField } = require("../sequelize");
+const { Template, TemplateField, TemplateFieldOption } = require("../sequelize");
 const { isRequestEmpty } = require("../utils/requests.utils");
 const { parseToJSONObject, fieldOptionCheckDefaults } = require("../utils/general.utils");
 const hash = require("object-hash");
@@ -8,8 +8,17 @@ exports.getTemplate = async (req, res) => {
 
     const template = await Template.findOne({
         where: { id: templateId },
-        include: [{ model: TemplateField, as: "fields" }],
-        order: [["fields", "position", "ASC"]],
+        include: [
+            {
+                model: TemplateField,
+                as: "fields",
+                include: [{ model: TemplateFieldOption, as: "options" }],
+            },
+        ],
+        order: [
+            ["fields", "position", "ASC"],
+            ["fields", "options", "position", "ASC"],
+        ],
     });
 
     const templateVersions = await Template.findAll({
@@ -36,7 +45,14 @@ exports.getTemplate = async (req, res) => {
 };
 
 exports.getVersion = async (req, res) => {
+    const { versionNumber } = req.params;
 
+    if (!versionNumber) {
+        console.log("No version number provided.");
+        return
+    }
+
+    console.log(versionNumber);
 };
 
 exports.createTemplate = async (req, res) => {
@@ -46,6 +62,12 @@ exports.createTemplate = async (req, res) => {
     }
 
     const { name, description, fields, versionOf } = req.body;
+    let version = 0;
+
+    if (versionOf) {
+        const templateLatestVersion = await Template.max("version", { where: { versionOf } });
+        version = templateLatestVersion + 1;
+    }
 
     let fieldPosition = 0;
     for (const field of fields) {
@@ -56,12 +78,17 @@ exports.createTemplate = async (req, res) => {
 
         if (field.dataType === "option") {
             let optionPosition = 0;
+
+            if (!field.options) {
+                field.options = [];
+            }
+
             field.options = field.options.map(option => {
-                fieldOptionCheckDefaults(option);
                 return {
                     ...option,
                     position: optionPosition++,
                     value: hash.MD5([option.value, new Date().getTime()]),
+                    usage: 0,
                 };
             });
         }
@@ -75,11 +102,18 @@ exports.createTemplate = async (req, res) => {
         {
             name,
             description,
-            fields,
+            version,
             versionOf,
+            fields,
         },
         {
-            include: [{ model: TemplateField, as: "fields" }],
+            include: [
+                {
+                    model: TemplateField,
+                    as: "fields",
+                    include: [{ model: TemplateFieldOption, as: "options" }],
+                },
+            ],
         },
     );
 
