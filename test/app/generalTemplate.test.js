271359c869801cccf010267924adb20f17b48fb4
Denish Sharma <denishsharma@ternaengg.ac.in>
2022-08-13 12:19:37 +0530

    refactor(test): refactor the general template test cases
    refactoring the test cases for general templates
    
    Author: Denish Sharma

--- test/app/generalTemplate.test.js
+++ test/app/generalTemplate.test.js
@@ -1,82 +1,114 @@
-const { Template, TemplateField } = require("../../app/sequelize");
+const { Template, TemplateField, TemplateFieldOption } = require("../../app/sequelize");
 const { parseToJSONObject } = require("../../app/utils/general.utils");
-const bcrypt = require("bcrypt");
 
-const templateStructure = {
-    id: "",
-    name: "",
-    description: "",
-    version: "",
-    fields: {
-        fieldSignature: {
-            id: "",
-            name: "",
-            dataType: "",
-            position: 0,
-            isRequired: false,
-            isFixed: false,
-        },
-    },
+/* How template works?
+ * For every form, there is a template that is used to create the form.
+ * The template is a JSON object that contains the fields and their options.
+ *
+ * There is always a root or base template, which you cannot delete or edit.
+ * You can always create a new version of the root template with the additional fields.
+ * You cannot delete or edit fields from root template, it will carry on to its versions.
+ *
+ * You can always edit the versions of the root template if not in use.
+ * If template is in use then you cannot edit the fields of that template, but you can edit the unused options of the fields.
+ */
+
+const updateTemplateUsage = async (templateId, usage) => {
+    const template = await Template.findOne({ where: { id: templateId } });
+    template.usage = usage;
+    await template.save();
+};
+const increaseTemplateUsage = async (templateId) => {
+    const template = await Template.findOne({ where: { id: templateId } });
+    template.usage++;
+    await template.save();
 };
 
-const seedTemplates = async () => {
-    const templateAttributes = {
-        name: "Template 1",
-        description: "Template 1 Description",
-        version: "1.0.0",
-    };
-
-    const template = await Template.create(templateAttributes);
-
-    const templateFieldAttributes = [
+// Test for creating a template with fields and versions... OK!
+const testCreateTemplateWithFieldsAndVersions = async () => {
+    const template = await Template.create(
         {
-            templateId: template.id,
-            signature: bcrypt.hashSync("field1", 10),
-            name: "field1",
-            dataType: "string",
-            position: 1,
-            isRequired: true,
-            isFixed: true,
+            name: "Template 2",
+            description: "Template 2 Description",
+            versions: [
+                {
+                    name: "Template 2 Version 1",
+                    description: "Template 2 Version 1 Description",
+                    fields: [
+                        {
+                            signature: "field1_sig",
+                            name: "field1",
+                            dataType: "string",
+                            isRequired: true,
+                            position: 0,
+                        },
+                    ],
+                },
+            ],
+            fields: [
+                {
+                    signature: "field1_sig",
+                    name: "field1",
+                    dataType: "string",
+                    isRequired: true,
+                    position: 0,
+                },
+                {
+                    signature: "field2_sig",
+                    name: "field2",
+                    dataType: "option",
+                    isRequired: true,
+                    position: 1,
+                    options: [
+                        {
+                            name: "option1",
+                            value: "option1",
+                            isDefault: true,
+                            position: 0,
+
+                        },
+                        {
+                            name: "option2",
+                            value: "option2",
+                            default: false,
+                            position: 1,
+                        },
+                    ],
+                },
+            ],
         },
         {
-            templateId: template.id,
-            signature: bcrypt.hashSync("field2", 10),
-            name: "field2",
-            dataType: "string",
-            position: 2,
-            isRequired: true,
-            isFixed: true,
+            include: [
+                {
+                    model: TemplateField,
+                    as: "fields",
+                    include: [
+                        {
+                            model: TemplateFieldOption,
+                            as: "options",
+                        },
+                    ],
+                },
+                {
+                    model: Template,
+                    as: "versions",
+                    include: [
+                        {
+                            model: TemplateField,
+                            as: "fields",
+                        },
+                    ],
+                },
+            ],
         },
-        {
-            templateId: template.id,
-            signature: bcrypt.hashSync("field3", 10),
-            name: "field3",
-            dataType: "string",
-            position: 3,
-            isRequired: true,
-            isFixed: true,
-        },
-    ];
+    );
 
-    for (const templateFieldAttribute of templateFieldAttributes) {
-        await TemplateField.create(templateFieldAttribute);
-    }
-};
-
-const someTest = async () => {
-    // Uncomment this when running test for first time
-    // await seedTemplates();
-
-    const templates = await Template.findAll({
-        include: [{ model: TemplateField, as: "fields" }],
-    });
-
-    console.log(parseToJSONObject(templates)[0].fields);
+    console.log(parseToJSONObject(template));
 };
 
 exports.test = async () => {
-    await someTest();
-
+    // await testCreateTemplateWithFieldsAndVersions(); //OK!
+    
     process.exit();
 };
 
