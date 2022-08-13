7cc0afeae6c807017f03abaeaa0632e6d1a0404c
Denish Sharma <denishsharma@ternaengg.ac.in>
2022-08-13 12:20:52 +0530

    update(sequelize-model): add register methods logic
    adding logic to register model methods on initialization
    
    Author: Denish Sharma

--- app/utils/general.utils.js
+++ app/utils/general.utils.js
@@ -1,3 +1,23 @@
 exports.parseToJSONObject = (obj) => {
     return JSON.parse(JSON.stringify(obj));
 };
+
+exports.addTimestamps = (obj) => {
+    if (!obj.createdAt && !obj.createdAt) {
+        obj.createdAt = new Date();
+        obj.updatedAt = new Date();
+    }
+};
+
+exports.fieldOptionCheckDefaults = (option) => {
+    if (!option.default) {
+        option.default = false;
+    }
+    if (!option.usage) {
+        option.usage = 0;
+    }
+    if (!option.fixed) {
+        option.fixed = false;
+    }
+    this.addTimestamps(option);
+};
