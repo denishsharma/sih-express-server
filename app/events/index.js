a62e4f18adcffd9c34c9d8431e8a54796188033a
Denish Sharma <denishsharma@ternaengg.ac.in>
2022-08-18 02:16:24 +0530

    update(template-record): subscribe Records events
    Author: Denish Sharma

--- app/events/index.js
+++ app/events/index.js
@@ -4,11 +4,13 @@ const subscribedEvents = require("./events.subscription");
 const SimpleStorageEvents = require("./contracts/SimpleStorage.events");
 const AddNumbersEvents = require("./contracts/AddNumbers.events");
 const VolunteerProfileEvents = require("./contracts/VolunteerProfile.events");
+const RecordsEvents = require("./contracts/Records.events");
 
 // Import Event Handlers
 const SimpleStorageEventHandlers = require("./handlers/SimpleStorage.events.handler");
 const AddNumbersEventHandlers = require("./handlers/AddNumbers.events.handler");
 const VolunteerProfileEventHandlers = require("./handlers/VolunteerProfile.events.handler");
+const RecordsEventHandlers = require("./handlers/Records.events.handler");
 
 const listen = () => {
     // Subscribe or Listen to events
@@ -20,6 +22,8 @@ const listen = () => {
     VolunteerProfileEvents["profileAdded"].subscribe({}, VolunteerProfileEventHandlers.profileAdded);
     VolunteerProfileEvents["profileUpdated"].subscribe({}, VolunteerProfileEventHandlers.profileUpdated);
     VolunteerProfileEvents["editorAccessUpdated"].subscribe({}, VolunteerProfileEventHandlers.editorAccessUpdated);
+
+    RecordsEvents["LogRecord"].subscribe({}, RecordsEventHandlers.logRecord);
 };
 
 module.exports = {
